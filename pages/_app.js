4d181213cce153076fe02f5cc4b43c18805984a8
Lev1nSvat <89538630+Lev1nSvat@users.noreply.github.com>
2022-09-28 17:47:03 +0300

    production

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const el = useRef();
+  useEffect(()=> {
+    gsap.from(el.current, {autoAlpha:0})
+
+  })
+
+  return (
+    <div ref={el} className='invisible'>
+      <Component {...Component} el={el} />
+    </div>
+  )
 }
 
 export default MyApp
